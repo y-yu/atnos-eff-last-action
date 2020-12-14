eab010c2cff050c505822b9ce2a261afcc5af903
Yoshimura Yuu <yyu@mental.poker>
2020-12-14 18:51:58 +0900

    Add the applicative test for `Task`

--- src/test/scala/org/atnos/eff/last/LastActionEffectTest.scala
+++ src/test/scala/org/atnos/eff/last/LastActionEffectTest.scala
@@ -1,14 +1,22 @@
 package org.atnos.eff.last
 
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import cats.Eval
 import monix.eval.Task
+import monix.execution.Scheduler
 import org.atnos.eff.Eff
 import org.atnos.eff.Fx
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.flatspec.AnyFlatSpec
 import scala.collection.mutable.ListBuffer
 import org.atnos.eff.last.LastActionEffect._
+import scala.collection.concurrent.TrieMap
 import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
 
 class LastActionEffectTest extends AnyFlatSpec with Diagrams {
@@ -133,6 +141,68 @@ class LastActionEffectTest extends AnyFlatSpec with Diagrams {
 
     assert(actual === 6)
     assert(listBuffer.toList === List(1, 2, 3, 4, 5, 6))
+  }
+
+  it should "run the last actions even if use monix async `Task` together" in {
+    import org.atnos.eff.addon.monix.task._
+
+    val threadPool: ExecutorService = Executors.newFixedThreadPool(10)
+    val executor: Executor = ExecutionContext.fromExecutorService(threadPool)
+    val ec: ExecutionContextExecutor = ExecutionContext.fromExecutor(executor)
+    implicit val scheduler: Scheduler = Scheduler(ec)
+
+    type R = Fx.fx2[LastAction, Task]
+
+    val count = new AtomicInteger(1)
+    val trieMap = new TrieMap[Int, Int]
+
+    val eff = for {
+      a <- taskDelay[R, Int] {
+        trieMap.addOne((count.getAndIncrement(), 1))
+        1
+      }
+      _ <- addLast[R](
+        trieMap.addOne((count.getAndIncrement(), 5))
+      )
+      // Applicative composition of `Task`
+      b <- taskDelay[R, Int] {
+        Thread.sleep(10)
+        trieMap.addOne((count.getAndIncrement(), 2))
+        2
+      } *> taskDelay[R, Int] {
+        Thread.sleep(10)
+        trieMap.addOne((count.getAndIncrement(), 3))
+        3
+      }
+      _ <- addLast[R](
+        trieMap.addOne((count.getAndIncrement(), 6))
+      )
+      c <- taskDelay[R, Int] {
+        trieMap.addOne((count.getAndIncrement(), 4))
+        4
+      }
+      _ <- addLast[R](
+        trieMap.addOne((count.getAndIncrement(), 7))
+      )
+    } yield a + b + c
+
+    val result = new TrieMap[List[Int], Int]
+
+    (1 to 50).foreach { _ =>
+      count.set(1)
+      trieMap.clear()
+
+      val actual = Await.result(
+        runAsync(eff.runLast).runToFuture,
+        5.seconds
+      )
+      assert(actual === 8)
+      result.updateWith(trieMap.toList.sortBy(_._2).map(_._1))(v => v.map(_ + 1).orElse(Some(1)))
+    }
+
+    // We expect that the both case should be occurred in the challenges.
+    assert(result.contains(List(1, 2, 3, 4, 5, 6, 7)))
+    assert(result.contains(List(1, 3, 2, 4, 5, 6, 7)))
   }
 
   it should "run from inner action rather than outer one" in {
